6c8b88c60953b250ab432e856840b548d944e628
abydenes <schoolreef2@protonmail.com>
2023-02-24 23:23:38 +0300

    Lint code

--- src/data/products.js
+++ src/data/products.js
@@ -1,120 +1,120 @@
-import frankZhang from "../assets/frank-zhang.jpg";
-import joeGreen from "../assets/joe-green.jpg";
-import mcheLee from "../assets/mche-lee.jpg";
-import rodrigoDosReis from "../assets/rodrigo-dos-reis.jpg";
-import rosannaGaddoni from "../assets/rosanna-gaddoni.jpg";
-import shelleyPauls from "../assets/shelley-pauls.jpg";
-import xuanNguyen from "../assets/xuan-nguyen.jpg";
-import blackGarlic from "../assets/black-garlic.jpg";
-import mixedGarlic from "../assets/mixed-garlic.jpg";
-import growingGarlic from "../assets/growing-garlic.jpg";
-import slicedGarlic from "../assets/sliced-garlic.jpg";
-import chineseGarlic from "../assets/chinese-garlic.jpg";
-import peeledGarlic from "../assets/peeled-garlic.jpg";
+import frankZhang from '../assets/frank-zhang.jpg'
+import joeGreen from '../assets/joe-green.jpg'
+import mcheLee from '../assets/mche-lee.jpg'
+import rodrigoDosReis from '../assets/rodrigo-dos-reis.jpg'
+import rosannaGaddoni from '../assets/rosanna-gaddoni.jpg'
+import shelleyPauls from '../assets/shelley-pauls.jpg'
+import xuanNguyen from '../assets/xuan-nguyen.jpg'
+import blackGarlic from '../assets/black-garlic.jpg'
+import mixedGarlic from '../assets/mixed-garlic.jpg'
+import growingGarlic from '../assets/growing-garlic.jpg'
+import slicedGarlic from '../assets/sliced-garlic.jpg'
+import chineseGarlic from '../assets/chinese-garlic.jpg'
+import peeledGarlic from '../assets/peeled-garlic.jpg'
 
 export const products = [
   {
     id: 0,
-    name: "Small garlic",
-    desc: "Ut id sagittis sem. Suspendisse aliquam bibendum nunc, nec elementum nunc tempor eget. Vestibulum sed aliquet ligula. Nulla sit amet.",
+    name: 'Small garlic',
+    desc: 'Ut id sagittis sem. Suspendisse aliquam bibendum nunc, nec elementum nunc tempor eget. Vestibulum sed aliquet ligula. Nulla sit amet.',
     price: 5,
     src: frankZhang,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 1,
-    name: "Purple garlic",
-    desc: "Aenean et mi fermentum, tincidunt ligula vitae, vehicula felis. Duis et mauris sapien. Ut ut nisi vel lorem dapibus facilisis.",
+    name: 'Purple garlic',
+    desc: 'Aenean et mi fermentum, tincidunt ligula vitae, vehicula felis. Duis et mauris sapien. Ut ut nisi vel lorem dapibus facilisis.',
     price: 7,
     src: joeGreen,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 2,
-    name: "White garlic",
-    desc: "Pellentesque metus ligula, finibus vel vestibulum ut, commodo eget tellus. Morbi tristique sodales tortor at imperdiet. Morbi tempus mauris quis.",
+    name: 'White garlic',
+    desc: 'Pellentesque metus ligula, finibus vel vestibulum ut, commodo eget tellus. Morbi tristique sodales tortor at imperdiet. Morbi tempus mauris quis.',
     price: 3,
     src: mcheLee,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 3,
-    name: "Red garlic heads",
-    desc: "In interdum diam ipsum, ut ullamcorper metus varius nec. Aliquam erat volutpat. Maecenas vehicula augue et viverra hendrerit. Integer ex.",
+    name: 'Red garlic heads',
+    desc: 'In interdum diam ipsum, ut ullamcorper metus varius nec. Aliquam erat volutpat. Maecenas vehicula augue et viverra hendrerit. Integer ex.',
     price: 8,
     src: rodrigoDosReis,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 4,
-    name: "Wall garlic",
-    desc: "In cursus porttitor magna, sit amet aliquam arcu scelerisque sed. Cras magna dui, facilisis congue euismod faucibus, ultrices bibendum nulla.",
+    name: 'Wall garlic',
+    desc: 'In cursus porttitor magna, sit amet aliquam arcu scelerisque sed. Cras magna dui, facilisis congue euismod faucibus, ultrices bibendum nulla.',
     price: 10,
     src: rosannaGaddoni,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 5,
-    name: "Fresh garlic",
-    desc: "Proin ac nisi vitae lorem lacinia iaculis ut non magna. Curabitur iaculis quis dui in lobortis. Nunc mattis ut dolor.",
+    name: 'Fresh garlic',
+    desc: 'Proin ac nisi vitae lorem lacinia iaculis ut non magna. Curabitur iaculis quis dui in lobortis. Nunc mattis ut dolor.',
     price: 10,
     src: shelleyPauls,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 6,
-    name: "Purple garlic heads",
-    desc: "Morbi molestie odio ac ante feugiat cursus. Fusce pretium ipsum id ex consequat consectetur. Vivamus tincidunt eu justo a efficitur.",
+    name: 'Purple garlic heads',
+    desc: 'Morbi molestie odio ac ante feugiat cursus. Fusce pretium ipsum id ex consequat consectetur. Vivamus tincidunt eu justo a efficitur.',
     price: 6,
     src: xuanNguyen,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 7,
-    name: "Black garlic",
-    desc: "Sed mollis placerat suscipit. Morbi vehicula massa et vulputate egestas. Ut aliquet aliquam lorem sed aliquam. Nulla ac metus ante.",
+    name: 'Black garlic',
+    desc: 'Sed mollis placerat suscipit. Morbi vehicula massa et vulputate egestas. Ut aliquet aliquam lorem sed aliquam. Nulla ac metus ante.',
     price: 11,
     src: blackGarlic,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 8,
-    name: "Mixed garlic",
-    desc: "Fusce vel sapien vehicula, venenatis sapien id, venenatis lectus. Praesent vel condimentum tellus, nec aliquam ligula. In facilisis arcu libero.",
+    name: 'Mixed garlic',
+    desc: 'Fusce vel sapien vehicula, venenatis sapien id, venenatis lectus. Praesent vel condimentum tellus, nec aliquam ligula. In facilisis arcu libero.',
     price: 7,
     src: mixedGarlic,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 9,
-    name: "Growing garlic",
-    desc: "Nunc dapibus ornare dolor. Nunc iaculis tincidunt magna. Praesent ut sapien felis. Nunc porta pellentesque pretium. Maecenas elementum ut sem.",
+    name: 'Growing garlic',
+    desc: 'Nunc dapibus ornare dolor. Nunc iaculis tincidunt magna. Praesent ut sapien felis. Nunc porta pellentesque pretium. Maecenas elementum ut sem.',
     price: 50,
     src: growingGarlic,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 10,
-    name: "Sliced garlic",
-    desc: "Maecenas vehicula ligula in felis tempus, sit amet vestibulum justo malesuada. Aliquam facilisis tellus neque, tincidunt convallis enim mattis et.",
+    name: 'Sliced garlic',
+    desc: 'Maecenas vehicula ligula in felis tempus, sit amet vestibulum justo malesuada. Aliquam facilisis tellus neque, tincidunt convallis enim mattis et.',
     price: 15,
     src: slicedGarlic,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 11,
-    name: "Chinese garlic",
-    desc: "Ut placerat mollis nunc, id maximus massa accumsan vitae. Vivamus sed fringilla nibh, non placerat nisl. Etiam fermentum eros non.",
+    name: 'Chinese garlic',
+    desc: 'Ut placerat mollis nunc, id maximus massa accumsan vitae. Vivamus sed fringilla nibh, non placerat nisl. Etiam fermentum eros non.',
     price: 5,
     src: chineseGarlic,
-    quantity: 0,
+    quantity: 0
   },
   {
     id: 12,
-    name: "Peeled garlic",
-    desc: "Maecenas fringilla metus sed urna semper, ac ornare mauris rhoncus. Donec in ultrices eros, et malesuada arcu. Nullam ac sapien.",
+    name: 'Peeled garlic',
+    desc: 'Maecenas fringilla metus sed urna semper, ac ornare mauris rhoncus. Donec in ultrices eros, et malesuada arcu. Nullam ac sapien.',
     price: 17,
     src: peeledGarlic,
-    quantity: 0,
-  },
-];
+    quantity: 0
+  }
+]
